0b77777d2a80adff2a15dba659e558aed971ea0f
Caila Bridges <146654762+cailabridges@users.noreply.github.com>
2024-01-08 19:16:57 -0500

    Add files via upload

---
+++ client/src/context/authContext.jsx
@@ -0,0 +1,54 @@
+// authContext.jsx
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8800/api/auth/login",
+        inputs,
+        { withCredentials: true }
+      );
+      setCurrentUser(res.data);
+      localStorage.setItem("user", JSON.stringify(res.data));
+    } catch (error) {
+      console.error("Login failed:", error);
+      throw error;
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post("http://localhost:8800/api/auth/logout");
+      setCurrentUser(null);
+      localStorage.removeItem("user");
+    } catch (error) {
+      console.error("Logout failed:", error);
+      throw error;
+    }
+  };
+
+  const updateUser = (updatedUser) => {
+    const updatedUserData = updatedUser.img
+      ? updatedUser
+      : { ...currentUser, ...updatedUser };
+
+    setCurrentUser(updatedUserData);
+    localStorage.setItem("user", JSON.stringify(updatedUserData));
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUser, setCurrentUser, login, logout, updateUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};

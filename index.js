0c861ec272c2ea744c97a74a35078322f55a26c1
Caila Bridges <bridgescaila@gmail.com>
2024-02-21 19:50:16 -0500

    my second commit

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
-import postRoutes from "./routes/posts.js";
-import cookieParser from "cookie-parser";
-import multer from "multer";
-import path from "path"; // Import path module to resolve file paths
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/users.js");
+const postRoutes = require("./routes/posts.js");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend domain
-    credentials: true, // This is important for cookies
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.json());
 
-// Resolve the absolute path to the upload directory
 const uploadDir = path.resolve(__dirname, "../client/dist/upload");
 
-// Multer configuration for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir); // Use the resolved upload directory path
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
   },
 });
 
-// Express route to handle image upload
 app.post("/api/upload", (req, res) => {
   const userImageUpload = multer({ storage: storage }).single("file");
 
@@ -41,17 +38,16 @@ app.post("/api/upload", (req, res) => {
         .status(500)
         .json({ message: "Error uploading image", error: err });
     }
-    // Assuming the image was successfully uploaded and stored
-    // Return the URL where the image is stored
     res.status(200).json({ imageUrl: `${req.file.filename}` });
   });
 });
 
-// Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
-  console.log("Server connected and listening on port 8800!");
+const PORT = process.env.PORT || 8800; // Use the port provided by Heroku or default to 8800
+
+app.listen(PORT, () => {
+  console.log(`Server connected and listening on port ${PORT}!`);
 });

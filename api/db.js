6a9eda0342bcfe76a1688065db7a1a5669758eb0
Caila Bridges <bridgescaila@gmail.com>
2024-02-24 13:59:45 -0500

    Update mysql connection and api port

--- api/db.js
+++ api/db.js
@@ -1,8 +1,17 @@
 import mysql from "mysql";
 
+// export const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "TallSloth29!",
+//   database: "blog",
+// });
+
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "TallSloth29!",
-  database: "blog",
+  host: "us-cluster-east-01.k8s.cleardb.net",
+  user: "b44a045a7455b4",
+  password: "9de1d90b",
+  database: "heroku_7c11955d935d599",
 });
+
+// mysql://b44a045a7455b4:9de1d90b@us-cluster-east-01.k8s.cleardb.net/heroku_7c11955d935d599?reconnect=true
--- api/index.js
+++ api/index.js
@@ -10,14 +10,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend domain
-    credentials: true, // This is important for cookies
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.json());
 
-// Multer configuration for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../client/dist/upload");
@@ -27,7 +26,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// Express route to handle image upload
 app.post("/api/upload", (req, res) => {
   const userImageUpload = multer({ storage: storage }).single("file");
 
@@ -37,17 +35,15 @@ app.post("/api/upload", (req, res) => {
         .status(500)
         .json({ message: "Error uploading image", error: err });
     }
-    // Assuming the image was successfully uploaded and stored
-    // Return the URL where the image is stored
+
     res.status(200).json({ imageUrl: `${req.file.filename}` });
   });
 });
 
-// Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log("Server connected and listening on port 8800!");
 });
